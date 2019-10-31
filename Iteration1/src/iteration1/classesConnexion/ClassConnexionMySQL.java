09fd855c7e57a001c89ad9456d0b44b161f3fd08
Paul <paul.gaido@yahoo.com>
2019-10-31 18:22:13 +0100

    ajout connexion mysql

--- Iteration1/src/iteration1/classesConnexion/ClassConnexionMySQL.java
+++ Iteration1/src/iteration1/classesConnexion/ClassConnexionMySQL.java
@@ -6,21 +6,44 @@
 package iteration1.classesConnexion;
 import iteration1.forms.FormConnect;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+
+
 /**
  *
  * @author Paul
  */
 public class ClassConnexionMySQL extends ClassConnexion{ 
     
+    private Connection connecter = null;
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
    
     public ClassConnexionMySQL(){}
     
    
     public void connexion(String user, String psswd) {
         
-        
-        
-        
+        try {
+            // chargement driver sql
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            // setup connexion avec la BD (A CHANGER ICI BD LOCALE WAMPSERVEUR POUR TEST)
+            connecter = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/test"
+                            + "user=" + user + "&password=" + psswd);
+
+        } catch (Exception e) {
+           System.out.println(e);
+        }
     }
     
 }
